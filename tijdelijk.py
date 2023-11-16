ece0b4fbf5f66565a4e1db4e1ee1e43faaf23a39
Vicky Verlinden <vicky_verlinden@hotmail.com>
2023-11-16 20:38:02 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -5,9 +5,11 @@ prijzen = {
     }
 aanbieding = prijzen["vanille"]*0.8
 reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €"
-#oefening 5 klopt niet, dit is berekend op basis van de waarden van aardbei-ijs, maar de vraag is vanille-ijs
+#oefening 5 klopt niet, dit is berekend op basis van de waarden van aardbei-ijs, maar de vraag is vanille-ijs (reclame_tekst2)
+reclame_tekst3 = "Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€"
+print(("Vandaag in de aanbieding : vanille-ijs, 1 liter - slechts €3,20").upper())
 reclame_tekst4 = ["Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€"]
-el = "Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€"
+el = "Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€", "3,20"
 for i in el:
     print(i)
 print(("el").lower())
