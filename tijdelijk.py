18f165304742bf3db9c658180266b55f2a3392b6
Vicky Verlinden <vicky_verlinden@hotmail.com>
2023-11-18 16:22:24 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-    }
-aanbieding = prijzen["vanille"]*0.8
-reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €"
-#oefening 5 klopt niet, dit is berekend op basis van de waarden van aardbei-ijs, maar de vraag is vanille-ijs (reclame_tekst2)
-reclame_tekst3 = "Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€"
-print(("Vandaag in de aanbieding : vanille-ijs, 1 liter - slechts €3,20").upper())
-reclame_tekst4 = ["Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€"]
-el = "Vandaag", "in", "de", "aanbieding", ":", "vanille-ijs", ",", "1", "liter", "-", "slechts", "€", "3,20"
-for i in el:
-    print(i)
-print(("el").lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+        }
+
+    aanbieding = prijzen["aardbei"]*0.8
+
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+    reclame_tekst2 = reclame_tekst[:63]
+
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else: 
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
